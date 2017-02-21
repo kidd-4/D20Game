383fa5a6760650d4f1f718928648e474136393ce
grey <GreyLee369@gmail.com>
2017-02-21 18:30:53 -0500

    "update the Maplistener"

--- D20Game/src/actionListener/MapListener.java
+++ D20Game/src/actionListener/MapListener.java
@@ -4,22 +4,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
-
+import characters.Cells;
 import map.Map;
+import enumclass.TileType;
 
 public class MapListener implements ActionListener{
 	public Map map;
-	int[][] newMap;
-	int rows;
-	int cols;
-	public MapListener(Map map,int rows,int cols) {
+//	int[][] newMap;
+	Cells[][] newMap;
+	
+	int x,y;
+	public MapListener(Map map) {
 		this.map = map;
-		this.rows = rows;
-		this.cols = cols;
+		
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -28,47 +26,46 @@ public class MapListener implements ActionListener{
 			 
 			public void keyPressed(KeyEvent e){
 				if(e.getKeyCode()==KeyEvent.VK_0){
-					int x = (int) jButton.getClientProperty("Rows");
-					int y = (int) jButton.getClientProperty("Cols");
+					 x = (int) jButton.getClientProperty("Rows");
+					 y = (int) jButton.getClientProperty("Cols");
 					newMap = map.getMap();
-					newMap[x][y] =0;
+					newMap[x][y].setTileType(TileType.Ground);
 					map.setMap(newMap);
 					map.drawMap(2);
 					System.out.println(jButton.getClientProperty("Rows")+" "+jButton.getClientProperty("Cols"));
 					}
 			else if(e.getKeyCode()==KeyEvent.VK_1){
-				int x = (int) jButton.getClientProperty("Rows");
-				int y = (int) jButton.getClientProperty("Cols");
+				 x = (int) jButton.getClientProperty("Rows");
+				 y = (int) jButton.getClientProperty("Cols");
 				newMap = map.getMap();
-				newMap[x][y] =1;
+				newMap[x][y].setTileType(TileType.WALL);
 				map.setMap(newMap);
 				map.drawMap(2);
-				System.out.println(jButton.getClientProperty("Rows")+" "+jButton.getClientProperty("Cols"));
 				}
 			else if(e.getKeyCode()==KeyEvent.VK_2)
 			{
-				int x = (int) jButton.getClientProperty("Rows");
-				int y = (int) jButton.getClientProperty("Cols");
+				 x = (int) jButton.getClientProperty("Rows");
+				 y = (int) jButton.getClientProperty("Cols");
 				newMap = map.getMap();
-				newMap[x][y] =2;
+				newMap[x][y].setTileType(TileType.CHEST);
 				map.setMap(newMap);
 				map.drawMap(2);
 			}
 			else if(e.getKeyCode()==KeyEvent.VK_3)
 			{
-				int x = (int) jButton.getClientProperty("Rows");
-				int y = (int) jButton.getClientProperty("Cols");
+				 x = (int) jButton.getClientProperty("Rows");
+				 y = (int) jButton.getClientProperty("Cols");
 				newMap = map.getMap();
-				newMap[x][y] =3;
+				newMap[x][y].setTileType(TileType.HERO);
 				map.setMap(newMap);
 				map.drawMap(2);
 			}
 			else if(e.getKeyCode()==KeyEvent.VK_4)
 			{
-				int x = (int) jButton.getClientProperty("Rows");
-				int y = (int) jButton.getClientProperty("Cols");
+				 x = (int) jButton.getClientProperty("Rows");
+				 y = (int) jButton.getClientProperty("Cols");
 				newMap = map.getMap();
-				newMap[x][y] =4;
+				newMap[x][y].setTileType(TileType.MONSTER);
 				map.setMap(newMap);
 				map.drawMap(2);
 			}
