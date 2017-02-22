629d34c8a6df45bba4c897215476ff5e3b426628
grey <GreyLee369@gmail.com>
2017-02-21 23:47:33 -0500

    "add LoadMap class"

---
+++ D20Game/src/load/LoadMap.java
@@ -0,0 +1,49 @@
+package load;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import characters.Cells;
+import enumclass.TileType;
+import map.Map;
+
+public class LoadMap {
+	Cells[][] cells;
+	
+	public Cells[][] loadMap(Map map, int count) throws IOException{
+		String filePath = "/Users/grey/Desktop/Test/Map/Map"+count+".txt";//different maps
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
+		
+		String string = null;
+		
+		String string2 = bufferedReader.readLine();
+		String[] strings2 = string2.split(" ");
+		int rows = Integer.parseInt(strings2[0]);
+		int cols = Integer.parseInt(strings2[1]);
+		cells = new Cells[rows][cols];
+		int i=0;
+//		System.out.println("rows "+rows+" cols"+cols);
+		
+		while((string = bufferedReader.readLine())!=null)
+		{
+			String[] strings = string.split(" ");
+			
+				for(int j=0;j<cols;j++)
+					cells[i][j] = new Cells(TileType.valueOf(strings[j]), rows,cols );
+				i++;//while loop
+		}
+		
+		
+//		for(int i=0;i<rows;i++)
+//			for(int j=0;j<cols;j++)
+//				System.out.println(cells[i][j].getTileType());
+		
+		bufferedReader.close();
+		return cells;
+		
+		
+	}
+
+
+
+}
