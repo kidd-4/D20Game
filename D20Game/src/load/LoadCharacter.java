989ada807c2954604b20a10175902226c7881174
grey <GreyLee369@gmail.com>
2017-02-23 22:54:04 -0500

    "change loadcharacter and loaditem"

--- D20Game/src/load/LoadCharacter.java
+++ D20Game/src/load/LoadCharacter.java
@@ -10,43 +10,61 @@ import characters.Items;
 import enumclass.Orientation;
 
 public class LoadCharacter {
+	ArrayList<Characters> arrayList = new ArrayList<Characters>();
+	ArrayList<Items> arrayList2 = new ArrayList<Items>();
+	Items weapon;
+	Items shield;
+	Items helmet;
+	Items armor;
+	Items ring;
+	Items belt;
+	Items boot;
+	Characters characters;
+	//在创建地图时，需要加入创建好的人物，这个时候需要将下拉框的名字和文件中的名字对应，回返对应的人物对象。读取人物时也需要
+	public Characters loadcharacter(String string) throws IOException{
+		String filePath = "/Users/grey/Desktop/Test/Characters.txt";
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
+		
+		String string2 = null;
+		while((string2 = bufferedReader.readLine())!= null){
+			String[] strings = string2.split(" ");
+			if(string.equals(strings[0])){
+				weapon = new Items(strings[7], Integer.parseInt(strings[8]));
+				shield = new Items(strings[9], Integer.parseInt(strings[10]));
+				helmet = new Items(strings[11], Integer.parseInt(strings[12]));
+				armor = new Items(strings[13], Integer.parseInt(strings[14]));
+				ring = new Items(strings[15], Integer.parseInt(strings[16]));
+				belt = new Items(strings[17], Integer.parseInt(strings[18]));
+				boot = new Items(strings[19], Integer.parseInt(strings[20]));
+				arrayList2.add(weapon);
+				arrayList2.add(shield);
+				arrayList2.add(helmet);
+				arrayList2.add(armor);
+				arrayList2.add(ring);
+				arrayList2.add(belt);
+				arrayList2.add(boot);
+				
+				characters = new Characters(strings[0], Integer.parseInt(strings[1]), Integer.parseInt(strings[2]), 
+						Integer.parseInt(strings[3]), Integer.parseInt(strings[4]), Integer.parseInt(strings[5]),
+						Enum.valueOf(Orientation.class, strings[6]), arrayList2);
+			}
+			
+		}
+		
+		bufferedReader.close();
+		return characters;
+	}
 	
-//	public String loadcharacter(String string) throws IOException{
-//		String filePath = "/Users/grey/Desktop/Test/Characters.txt";
-//		BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
-//		
-//		String string2 = null;
-//		String newString = null;
-//		while((string2 = bufferedReader.readLine())!= null){
-//			String[] strings = string2.split(" ");
-//			if(string.equals(strings[0]))
-//			newString = string2;
-//		}
-//		
-//		bufferedReader.close();
-//		return newString;
-//	}
-	
-	
+	//显示主界面中的下拉框中的人物列表
 	public ArrayList<Characters> readCharacter() throws IOException{
-		ArrayList<Characters> arrayList = new ArrayList<Characters>();
-		ArrayList<Items> arrayList2 = new ArrayList<Items>();
-		Items weapon;
-		Items shield;
-		Items helmet;
-		Items armor;
-		Items ring;
-		Items belt;
-		Items boot;
-		Characters characters;
+		
 		String filePath = "/Users/grey/Desktop/Test/Characters.txt";
 		BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
 		
 		String string = null;
 		while((string = bufferedReader.readLine())!= null){
 			String[] strings = string.split(" ");
-			for(int i=0;i<=20;i++)
-				System.out.println(strings[i]);
+			
 			weapon = new Items(strings[7], Integer.parseInt(strings[8]));
 			shield = new Items(strings[9], Integer.parseInt(strings[10]));
 			helmet = new Items(strings[11], Integer.parseInt(strings[12]));
--- D20Game/src/load/LoadItem.java
+++ D20Game/src/load/LoadItem.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import characters.Items;
 
 public class LoadItem {
-	
+	//在创建人物时，多选框选择一个item，则返回对应姓名的名字和值的字符串。读取人物时也需要
 	public String loadItem(String string) throws IOException{
 		String filePath = "/Users/grey/Desktop/Test/Items.txt";
 		BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
@@ -25,7 +25,7 @@ public class LoadItem {
 		return newString;
 		
 	}
-	
+	//显示主界面的下拉框中的物品列表
 	public ArrayList<Items> readItem() throws IOException{
 		ArrayList<Items> arrayList = new ArrayList<Items>();
 		Items items;
