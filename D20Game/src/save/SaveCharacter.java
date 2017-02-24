7bf1da334c1c878309279a0f6e7775b0ead87dd8
grey <GreyLee369@gmail.com>
2017-02-24 16:11:00 -0500

    "change the saveCharacter parameter"

--- D20Game/src/save/SaveCharacter.java
+++ D20Game/src/save/SaveCharacter.java
@@ -3,22 +3,21 @@ package save;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-
 import characters.Characters;
 import characters.Items;
 
 public class SaveCharacter {
 	
-	public void saveCharacter(ArrayList<Characters> arrayList) throws IOException{
+	public void saveCharacter(Characters characters) throws IOException{
 		String filePath = "/Users/grey/Desktop/Test/Characters.txt"; //save different maps
-		BufferedWriter bufferedWriter  = new BufferedWriter(new FileWriter(filePath));
+		BufferedWriter bufferedWriter  = new BufferedWriter(new FileWriter(filePath,true));
 		
-		for(Characters characters:arrayList){
 			bufferedWriter.write(characters.getName()+" ");
 			bufferedWriter.write(String.valueOf(characters.getLevel())+" ");
 			bufferedWriter.write(String.valueOf(characters.getHitpoints())+" ");
+			bufferedWriter.write(String.valueOf(characters.getMovement())+" ");
 			bufferedWriter.write(String.valueOf(characters.getStrength())+" ");
+			bufferedWriter.write(String.valueOf(characters.getDefence())+" ");
 			bufferedWriter.write(String.valueOf(characters.getDexterity())+" ");
 			bufferedWriter.write(String.valueOf(characters.getWisdom())+" ");
 			bufferedWriter.write(characters.getOrient().toString()+" ");
@@ -29,7 +28,7 @@ public class SaveCharacter {
 			
 			bufferedWriter.flush();
 			bufferedWriter.newLine();
-		}
+		
 		
 		
 		bufferedWriter.close();
