4d9c955791d4a4b42b110365ee4f17ffb03b0c3c
grey <GreyLee369@gmail.com>
2017-02-23 22:49:48 -0500

    "change Cells class"

--- D20Game/src/characters/Cells.java
+++ D20Game/src/characters/Cells.java
@@ -5,13 +5,98 @@ import enumclass.TileType;
 public class Cells {
 	
 	TileType tileType;
-	int xCoord, yCoord;
+	int x, y;//the total length of x and y
 	boolean isVisited;
+	Characters characters;
+	Entry entry;
+	Exit exit;
+	Ground ground;
+	Items items;
+	Wall wall;
 	
-	public Cells(TileType tileType){
+	
+
+	public Cells(TileType tileType,int x, int y,Wall wall){
 		this.tileType = tileType;
+		this.x = x;
+		this.y = y;
+		this.wall = wall;
 	}
 	
+	public Cells(TileType tileType,int x, int y,Entry entry){
+		this.tileType = tileType;
+		this.x = x;
+		this.y = y;
+		this.entry = entry;
+	}
+	public Cells(TileType tileType,int x, int y,Exit exit){
+		this.tileType = tileType;
+		this.x = x;
+		this.y = y;
+		this.exit = exit;
+	}
+	public Cells(TileType tileType,int x, int y,Ground ground){
+		this.tileType = tileType;
+		this.x = x;
+		this.y = y;
+		this.ground = ground;
+		}
+	public Cells(TileType tileType,int x, int y,Items items){
+		this.tileType = tileType;
+		this.x = x;
+		this.y = y;
+		this.items = items;
+	}
+
+	public Cells(TileType tileType,int x, int y,Characters characters){
+		this.tileType = tileType;
+		this.x = x;
+		this.y = y;
+		this.characters = characters;
+	}
+	
+	
+	
+	public Characters getCharacters() {
+		return characters;
+	}
+
+	public Entry getEntry() {
+		return entry;
+	}
+
+	public Exit getExit() {
+		return exit;
+	}
+
+	public Ground getGround() {
+		return ground;
+	}
+
+	public Items getItems() {
+		return items;
+	}
+
+	public Wall getWall() {
+		return wall;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
 	public TileType getTileType(){
 		return tileType;
 	}
